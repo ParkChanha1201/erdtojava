c8ba8e493d86a459cd42a671f0a5011650699eb4
박찬하 <chanhacraft@naver.com>
2021-06-28 12:23:43 +0900

    first commit

---
+++ src/test/java/com/chcraft/erdtojava/ErdtojavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chcraft.erdtojava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErdtojavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
