f5efff5d9ee1ae23e2fd22cd46e84de37b58f88b
박찬하 <chanhacraft@naver.com>
2021-06-28 13:57:52 +0900

    .

--- src/main/java/com/chcraft/erdtojava/api/ApiController.java
+++ src/main/java/com/chcraft/erdtojava/api/ApiController.java
@@ -1,8 +1,20 @@
 package com.chcraft.erdtojava.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.chcraft.erd.Entity;
+import com.chcraft.erd.utils.JavaFileGenerator;
 
 @RestController
 public class ApiController {
+	@Autowired
+	JavaFileGenerator jfg;
 
+	@PostMapping("/java")
+	public String toJava(@RequestBody Entity entity,@RequestBody String packagePath,@RequestBody boolean generateGetterAndSetter) {
+		return jfg.generateFileString(entity, packagePath, generateGetterAndSetter);
+	}
 }
