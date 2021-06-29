b19ec1dfcc42b04338b039313d8110d8c7c58cec
박찬하 <chanhacraft@naver.com>
2021-06-29 09:49:35 +0900

    ,

--- backend/src/main/java/com/chcraft/erd/utils/JavaFileGenerator.java
+++
@@ -1,7 +0,0 @@
-package com.chcraft.erd.utils;
-
-import com.chcraft.erd.Entity;
-
-public interface JavaFileGenerator{
-	public String generateFileString(Entity entity, String packagePath, boolean generateGetterAndSetter);
-}
