f65a1055c56c4f0867df9ba409aab26d5d5e964e
박찬하 <chanhacraft@naver.com>
2021-06-29 09:48:23 +0900

    .

---
+++ backend/src/main/java/com/chcraft/erdtojava/api/ApiController.java
@@ -0,0 +1,41 @@
+package com.chcraft.erdtojava.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.chcraft.erd.Entity;
+import com.chcraft.erd.Relationship;
+import com.chcraft.erd.utils.JavaFileGenerator;
+import com.chcraft.erd.utils.SQLFileGenerator;
+import com.chcraft.erdtojava.api.param.JavaParam;
+import com.chcraft.erdtojava.api.param.SQLParam;
+
+@RestController
+public class ApiController {
+	@Autowired
+	JavaFileGenerator jfg;
+	@Autowired
+	SQLFileGenerator sfg;
+
+	@PostMapping("/java")
+	public String toJava(@RequestBody JavaParam param) {
+		return jfg.generateFileString(param.getEntity(), param.getPackagePath(), param.isGenerateGetterAndSetter());
+	}
+
+	@PostMapping("/sql")
+	public String toSQL(@RequestBody SQLParam param) {
+		return sfg.generateFileString(param.getEntities(), param.getRelationships());
+	}
+
+	@PostMapping("/sql/entity")
+	public String entityToSQL(@RequestBody Entity entity) {
+		return sfg.generateEntity(entity);
+	}
+
+	@PostMapping("/sql/relationship")
+	public String relationshipToSQL(@RequestBody Relationship relationship) {
+		return sfg.generateRelationship(relationship);
+	}
+}
