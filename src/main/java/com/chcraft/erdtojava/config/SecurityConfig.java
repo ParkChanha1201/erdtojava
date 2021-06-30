5ad191dc3a5fff98e3178bdeccd4aa2665d639a5
박찬하 <chanhacraft@gmail.com>
2021-06-30 11:05:26 +0900

    .

--- src/main/java/com/chcraft/erdtojava/config/SecurityConfig.java
+++ src/main/java/com/chcraft/erdtojava/config/SecurityConfig.java
@@ -2,18 +2,19 @@ package com.chcraft.erdtojava.config;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+		http.csrf().disable();
 
 		http.authorizeRequests()
 		    .antMatchers("**/**").permitAll()
 			.antMatchers("/java").permitAll()
-			.antMatchers("/sql/*").permitAll();
+			.antMatchers("/sql/*").permitAll()
+			.and()
+			.formLogin().disable();
 
 	}
 
