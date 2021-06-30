ad4cb110626e97af8eb2a9d1a2a313ceea302a21
박찬하 <chanhacraft@gmail.com>
2021-06-30 11:39:40 +0900

    .

--- src/main/java/com/chcraft/erdtojava/config/CorsConfig.java
+++ src/main/java/com/chcraft/erdtojava/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer{
 	    public void addCorsMappings(CorsRegistry registry) {
 	        registry.addMapping("/**")
 	                .allowedOrigins("*")
-	                .allowedMethods("GET", "POST")
+	                .allowedMethods("*")
 	                .maxAge(3000);
 	  }
 }
