baf6a3e4e2b994b45aa4622228d2b8dfef0bb72d
박찬하 <chanhacraft@gmail.com>
2021-06-30 11:09:12 +0900

    .

--- src/main/java/com/chcraft/erdtojava/config/SecurityConfig.java
+++ src/main/java/com/chcraft/erdtojava/config/SecurityConfig.java
@@ -7,14 +7,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable();
-
-		http.authorizeRequests()
-		    .antMatchers("**/**").permitAll()
-			.antMatchers("/java").permitAll()
-			.antMatchers("/sql/*").permitAll()
-			.and()
-			.formLogin().disable();
+		http.cors().disable();
 
 	}
 
