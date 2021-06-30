c590d03f19f07ed71eb97614adbcf7114841c0b2
박찬하 <chanhacraft@gmail.com>
2021-06-30 11:45:06 +0900

    .

--- src/main/java/com/chcraft/erdtojava/config/CorsConfig.java
+++ src/main/java/com/chcraft/erdtojava/config/CorsConfig.java
@@ -8,7 +8,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig implements WebMvcConfigurer{
 	  @Override
 	    public void addCorsMappings(CorsRegistry registry) {
-	        registry.addMapping("/**")
+	        registry
+	        .addMapping("/**")
+	        .allowCredentials(true)
 	        .allowedHeaders("*")
             .allowedMethods("*")
             .maxAge(1800)
